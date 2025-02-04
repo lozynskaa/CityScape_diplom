de0cb1a59d3799353acebac1702b4238cdc91fd5
lozynskaa <oksanalozynska477@gmail.com>
2025-02-04 19:40:10 +0200

    chore: updated empty case for posts

--- src/app/(main)/post/page.tsx
+++ src/app/(main)/post/page.tsx
@@ -1,16 +1,21 @@
 "use client";
 
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import { Suspense, useCallback, useEffect, useRef } from "react";
 import Post from "~/app/_components/post-card";
+import { Button } from "~/app/_components/ui/button";
+import If from "~/app/_components/ui/if";
 import { FullPageSpinner } from "~/app/_components/ui/spinner";
 import { useDebounce } from "~/hooks/use-debounce";
 import { useWritableSearchParams } from "~/hooks/use-writable-search-params";
 import { api } from "~/trpc/react";
 
 export default function PostsListPage() {
+  const session = useSession();
   const { searchParams } = useWritableSearchParams();
   const debouncedSearch = useDebounce(searchParams.get("search") ?? "", 500);
-
+  const router = useRouter();
   const observerRef = useRef<HTMLDivElement | null>(null);
 
   const input = {
@@ -74,7 +79,30 @@ export default function PostsListPage() {
   if (data?.pages?.length && !data.pages.every((page) => page.posts.length)) {
     return (
       <div className="flex h-full w-full items-center justify-center">
-        <h1 className="text-2xl font-bold">No posts found</h1>
+        <If condition={session.status === "authenticated"}>
+          <h1 className="text-2xl font-bold">No posts found</h1>
+          <p className="mt-2 text-sm text-gray-600">
+            Please create company and post, then try again.
+          </p>
+          <Button
+            className="w-22 mt-4"
+            onClick={() => router.push("/settings/company/new-company")}
+          >
+            Create company
+          </Button>
+        </If>
+        <If condition={session.status === "unauthenticated"}>
+          <h1 className="text-2xl font-bold">No posts found</h1>
+          <p className="mt-2 text-sm text-gray-600">
+            Please sign in, create company and post, then try again.
+          </p>
+          <Button
+            className="w-22 mt-4"
+            onClick={() => router.push("/api/auth/sign-in")}
+          >
+            Sign in
+          </Button>
+        </If>
       </div>
     );
   }
